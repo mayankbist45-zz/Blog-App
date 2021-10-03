cfb0d5be7fa5cdb0bac5d6be6524963c6f781870
Mayank Bist <mayankbist45@gmail.com>
2021-10-03 17:33:34 +0530

    Add Router

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    console.log(res);
-                    if (!res.ok) {
-                        throw Error('Could Not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                })
-                .catch(err => {
-                    console.log(err.message);
-                })
-        }, 1000)
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
