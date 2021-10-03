7163518c5a1852b94911e5ea0260f42de92deb05
Mayank Bist <mayankbist45@gmail.com>
2021-10-03 13:27:45 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    console.log(res);
+                    if (!res.ok) {
+                        throw Error('Could Not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                })
+        }, 1000)
+    }, []);
+
+    return (
+        <div className="home">
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
+        </div>
+    );
+}
+
+export default Home
+
